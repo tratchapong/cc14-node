343c39e9a3bfd305a23950d7a7708a7d2023e537
tratchapong <tratchapong@gmail.com>
2023-05-08 21:47:16 +0700

    add qotd-cow

--- path01/app.js
+++ path01/app.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const util = require('./res')
 
-
 console.log(util.random())
 
 console.log('in app.js')
@@ -10,3 +9,8 @@ console.log(path.resolve())
 
 // path.resolve คือ working dir หลัก
 // __dirname คือ ตำแหน่งของไฟล์ที่ run อยู่ (ถึง run ผ่าน require ก็ตาม)
+
+let pjoin1 = path.join('do', 'rae','me')
+let pjoin2 = path.posix.join('do', 'rae','me')
+console.log(pjoin1)
+console.log(pjoin2)
